0c9bc3ac7e6cd2b716f7bd1a07b230abe997b768
nadinekhis <torontino123@gmail.com>
2020-10-02 18:25:29 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let min = Infinity
+  for (let i of array){
+    if (i < min) min = i
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let max = -Infinity
+  for (let i of array){
+    if (i > max) max = i
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let res = array.reduce(function (acc, cur) {
+    return acc + cur
+  })
+  return res / array.length
 }
